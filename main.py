38d87a5de2df11ee51ad618a8f76c7a5bf70ecbb
kukree <kulevgen32@gmail.com>
2019-07-24 00:47:19 +0600

    Add table creation window

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QLineEdit,
     QLabel,
-    QDialog
+    QDialog,
+    QMessageBox
 )
 from PyQt5.QtCore import Qt
 import sys
@@ -42,11 +43,15 @@ class MainWindow(QWidget):
 
         self.employeesTable = EmployeesTable()  # TEMP
 
+        addTabButton = QPushButton('+')
+        addTabButton.setFixedSize(35, 30)
+        addTabButton.clicked.connect(self.createTable)
+
         self.tabs = QTabWidget()
-        # TODO: Добавить кнопку создания вкладки (таблицы в базе данных)
+        self.tabs.setCornerWidget(addTabButton)
 
         self.mainLayout = QGridLayout()
-        self.mainLayout.addWidget(self.tabs, 0, 0, 5, 5)
+        self.mainLayout.addWidget(self.tabs, 0, 1, 5, 5)
         self.mainLayout.addWidget(groupBox, 0, 6)
 
         self.setLayout(self.mainLayout)
@@ -63,6 +68,70 @@ class MainWindow(QWidget):
             tables = self.dbEngine.table_names()
             for table in tables:
                 self.tabs.addTab(self.employeesTable, table)
+
+    def createTable(self):
+        newTableInput = NewTableInput(self)
+        response = newTableInput.exec_()
+        if response == QDialog.Accepted:
+            print('Meow!')  # TEMP
+
+
+class NewTableInput(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.initWindow()
+
+    def initWindow(self):
+        self.names = []
+
+        self.layout = QGridLayout()
+
+        self.okButton = QPushButton('Создать')
+        self.okButton.clicked.connect(self.createTable)
+
+        tableNameLabel = QLabel('Имя таблицы')
+        self.tableName = QLineEdit()
+
+        self.newColumnLabel = QLabel('Новая колонка')
+        self.newColumnButton = QPushButton('+')
+        self.newColumnButton.clicked.connect(self.addColumn)
+
+        self.lastColumnID = 1
+
+        self.layout.addWidget(tableNameLabel, 0, 0)
+        self.layout.addWidget(self.tableName, 1, 0)
+        self.layout.addWidget(self.newColumnLabel, 0, self.lastColumnID)
+        self.layout.addWidget(self.newColumnButton, 1, self.lastColumnID)
+
+        self.layout.addWidget(self.okButton, 2, 0, 1, self.lastColumnID+1)
+
+        self.setLayout(self.layout)
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setWindowTitle('Создание таблицы')
+        self.resize(500, 300)
+
+    def addColumn(self):
+        self.layout.removeWidget(self.newColumnLabel)
+        self.layout.removeWidget(self.newColumnButton)
+        newColumnNameLabel = QLabel('Имя колонки')
+        newColumnName = QLineEdit()
+        self.names.append(newColumnName)
+        self.lastColumnID += 1
+        self.layout.addWidget(newColumnNameLabel, 0, self.lastColumnID)
+        self.layout.addWidget(newColumnName, 1, self.lastColumnID)
+        self.lastColumnID += 1
+        self.layout.addWidget(self.newColumnLabel, 0, self.lastColumnID)
+        self.layout.addWidget(self.newColumnButton, 1, self.lastColumnID)
+        self.layout.removeWidget(self.okButton)
+        self.layout.addWidget(self.okButton, 2, 0, 1, self.lastColumnID + 1)
+
+    def createTable(self):
+        for name in self.names:
+            if name.text() == '':
+                QMessageBox.critical(
+                    self, 'Ошибка', 'Одно из полей осталось пустым', QMessageBox.Ok, QMessageBox.Ok)
+                return
+        self.accept()
 
 
 class DataBaseInput(QDialog):
